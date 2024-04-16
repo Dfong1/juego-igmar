105f54cd0c428683f3e554a684006d1b3df288d5
Daniel Fong <danifongm@gmail.com>
2024-04-16 13:42:03 -0600

    Cambios en api (config de mail), frontend casi terminado

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EstadisticaComponent } from './components/estadistica/estadistica.component';
+import { SearchingComponent } from './components/searching/searching.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'dashboard', component: DashboardComponent},
+    { path: 'estadisticas', component: EstadisticaComponent},
+    { path: 'search', component: SearchingComponent },
+    { path: '**', component: NotfoundComponent}
+];
---
+++ web/src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [ NavbarComponent, RouterLink ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
