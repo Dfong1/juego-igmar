b753491bdea7005e92f8e5cb575474a46312e9c2
Daniel Fong <danifongm@gmail.com>
2024-04-16 15:14:18 -0600

    Diseño en estadisticas

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { EstadisticaComponent } from './components/estadistica/estadistica.component';
+import { EstadisticasComponent } from './components/estadisticas/estadisticas.component';
 import { SearchingComponent } from './components/searching/searching.component';
 
 export const routes: Routes = [
@@ -11,7 +11,7 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'dashboard', component: DashboardComponent},
-    { path: 'estadisticas', component: EstadisticaComponent},
+    { path: 'estadisticas', component: EstadisticasComponent},
     { path: 'search', component: SearchingComponent },
     { path: '**', component: NotfoundComponent}
 ];
--- web/src/app/components/estadisticas/estadisticas.component.ts
+++ web/src/app/components/estadisticas/estadisticas.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-estadisticas',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './estadisticas.component.html',
   styleUrl: './estadisticas.component.css'
 })
